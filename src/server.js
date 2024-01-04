cbab4f554e7ac9c6177a9b4f1c02fb0afe3c5af6
Gustavo <User@SUB-MSOCIAL-VID>
2024-01-04 14:28:08 -0300

    Updating CORS policy

--- src/server.js
+++ src/server.js
@@ -23,14 +23,14 @@ const corsOptions = {
 // server.use(cors(corsOptions));
 server.use(cors());
 
-// server.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // Rutas
 
