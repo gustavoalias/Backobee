5b545628e080127e754555bc705e0f0960387cd6
Gustavo <User@SUB-MSOCIAL-VID>
2024-01-04 14:21:07 -0300

    Update: CORS Policy

--- src/server.js
+++ src/server.js
@@ -20,16 +20,17 @@ const corsOptions = {
   credentials: true, // Permite incluir credenciales en las solicitudes (por ejemplo, cookies, certificados SSL, etc.)
 };
 
-server.use(cors(corsOptions));
+// server.use(cors(corsOptions));
+server.use(cors());
 
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // Rutas
 
