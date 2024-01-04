610e4092193abd6935ac135c627054ef841faea9
Gustavo <User@SUB-MSOCIAL-VID>
2024-01-04 12:17:53 -0300

    Minor fixes about ports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/server.js");
 const { database } = require("./src/db.js");
 
 database.sync({ force: true }).then(() => {
-  server.listen(8080, () => {
+  server.listen(10001, () => {
     console.log("Server running on port 8080 and connected to DB");
   });
 });
